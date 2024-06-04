0dbf444d4cce01dc91a93e3caba7ecbdd326435a
felipesouza <felipesouza@coamo.com.br>
2024-06-04 16:36:49 -0300

    feat: add rooms to namespace

--- namespaces/namespaces.js
+++ namespaces/namespaces.js
@@ -7,11 +7,24 @@ const expressServer = app.listen(8001);
 
 const io = socketIo(expressServer);
 
-io.on('connection', (socket) => {
-  console.log(socket.id, "has connected");
+io.of("/").on('connection', (socket) => {
+  
+  socket.join('chat');
+  socket.join('adminChat');
+
+  io.of('/').to('chat').emit('welcomeToChatRoom', {});
+  io.of('/').to('chat').to('chat1').to('chat2').emit('welcomeToChatRoom', {});
+  io.of('/admin').emit('welcomeToAdminChatRoom', {});
+  
+  console.log(socket.id, "has connected !");
   
   socket.on('newMessageToServer', (dataFromClient) => {
     console.log(dataFromClient);
-    io.emit('newMessageToClients', {text:dataFromClient.text})
+    io.of("/").emit('newMessageToClients', {text:dataFromClient.text})
   });
+});
+
+io.of("/admin").on('connection', (socket) => {
+  console.log(socket.id, "has connected to admin !");
+  io.of("/admin").emit('newMessageToClientsFromAdmin', {})
 });
